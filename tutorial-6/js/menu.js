7602b8e7f71626031ab6faa8ff558a25352ce996
iamwillgray <iamwillgray@gmail.com>
2018-10-24 19:53:17 +0100

    Add files via upload
    onClick event now moves the content down

--- tutorial-6/js/menu.js
+++ tutorial-6/js/menu.js
@@ -12,5 +12,24 @@ function showMenu() {
  var openMenu = document.getElementById("desktopNav");
  console.log();
  openMenu.style.display= "block";
-
 }
+
+function navOverlay() {
+ var menuOverlay = document.getElementById("topNav");
+ console.log();
+ menuOverlay.style.position= "static";
+ menuOverlay.style.height= "auto";
+}
+
+
+/* var mobileNav = true;
+
+function showMenu() {
+    if (mobile) {
+      openMenu.style.display= "block";
+        mobileNav = false;
+    } else {
+      openMenu.style.display= "none";
+        mobileNav = true;
+    }
+} */
