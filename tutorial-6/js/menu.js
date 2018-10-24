91644af3679e6326ff30181e2c98fbe944b01712
iamwillgray <iamwillgray@gmail.com>
2018-10-24 19:29:34 +0100

    Add files via upload
    Hide and show the menu

---
+++ tutorial-6/js/menu.js
@@ -0,0 +1,16 @@
+/* function showMenu() {
+ var openMenu = document.getElementsByClassName("desktopNav");
+ console.log();
+ openMenu.style.visibility="visible";
+
+}
+
+*/
+
+
+function showMenu() {
+ var openMenu = document.getElementsByClassName("desktopNav");
+ console.log();
+ openMenu.style.display= "block";
+
+}
