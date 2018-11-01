9684e3a2cf66c5aedcd6ad9e7f5d1aea16d73ce8
iamwillgray <iamwillgray@gmail.com>
2018-11-01 23:00:10 +0000

    Add files via upload

--- pantegpark/js/main.js
+++ pantegpark/js/main.js
@@ -15,6 +15,27 @@ function showMenu() {
     //turns the display off
 }
 
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function openDropdown() {
+    document.getElementById("navDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
 
 /* // When the user scrolls down 20px from the top of the document, slide down the navbar
 window.onscroll = function() {scrollFunction()};
