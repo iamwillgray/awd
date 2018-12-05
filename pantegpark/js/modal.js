ee5f01f44a195087f5762c4071410b62b34ac775
iamwillgray <iamwillgray@gmail.com>
2018-12-05 19:40:19 +0000

    Add files via upload

---
+++ pantegpark/js/modal.js
@@ -0,0 +1,45 @@
+function chooseSlide(choice) {
+    var i;
+    var slides = document.getElementsByClassName("img-card");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    var slides = document.getElementsByClassName(choice);
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "block";
+    }
+}
+
+function openModal() {
+  document.getElementById('myModal').style.display = "block";
+}
+
+function closeModal() {
+  document.getElementById('myModal').style.display = "none";
+}
+
+var slideIndex = 1;
+
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+
+  showSlides(n);
+}
+
+function showSlides(n) {
+    slideIndex=n
+    console.log(n,slideIndex)
+
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex-1].style.display = "block";
+}
