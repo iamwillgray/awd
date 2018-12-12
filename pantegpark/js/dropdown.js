85a61f0cb7d884afb1e1e39f6d2dc3bbed6291c1
iamwillgray <iamwillgray@gmail.com>
2018-12-12 19:29:52 +0000

    Add files via upload

---
+++ pantegpark/js/dropdown.js
@@ -0,0 +1,12 @@
+//hide all tabs first
+$('.tab-content').hide();
+//show the first tab content
+$('#tab-1').show();
+
+$('#select-box').change(function () {
+ dropdown = $('#select-box').val();
+//first hide all tabs again when a new option is selected
+$('.tabcontent').hide();
+//then show the tab content of whatever option value was selected
+$('#' + "tab-" + dropdown).show();
+});
--- pantegpark/js/main.js
+++ pantegpark/js/main.js
@@ -1,3 +1,4 @@
+'main.js'
 // Menu
 function showMenu() {
     // i is finding the ID desktopNav in the HTML
@@ -146,3 +147,16 @@ document.getElementById( 'openAll' ).addEventListener( 'click', openAll);
 document.getElementById( 'closeAll' ).addEventListener( 'click', closeAll);
 
 ///////////////////////////////////////////
+
+//hide all tabs first
+$('.tab-content').hide();
+//show the first tab content
+$('#tab-1').show();
+
+$('#select-box').change(function () {
+ dropdown = $('#select-box').val();
+//first hide all tabs again when a new option is selected
+$('.tab-content').hide();
+//then show the tab content of whatever option value was selected
+$('#' + "tab-" + dropdown).show();
+});
